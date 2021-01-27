eb5571f7476341be38b109021e9b653b69e2e711
joyal0 <76865773+joyal0@users.noreply.github.com>
2021-01-27 22:15:02 +0530

    Add files via upload

---
+++ squareroot14.cpp
@@ -0,0 +1,13 @@
+//C++ program to find the square root of  a number
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int main()
+{
+    long long int a;
+    cout<<"Enter the number to find its square root ";
+    cin>>a;
+    cout<<"The square root of "<<a<<" is "<<sqrt(a)<<endl;
+    return 0;
+}
